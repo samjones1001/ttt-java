6e9097c432a2e401308ff545a1616c06157cc48e
Sam Jones <sam.jones494@googlemail.com>
2019-11-15 16:26:22 +0000

    add GameConfig class

---
+++ src/main/java/ttt/game/GameConfig.java
@@ -0,0 +1,29 @@
+package ttt.game;
+
+import ttt.player.Player;
+
+public class GameConfig {
+    private Player currentPlayer;
+    private Player opponent;
+    private Board board;
+
+    public GameConfig(Player currentPlayer, Player opponent, Board board) {
+        this.currentPlayer = currentPlayer;
+        this.opponent = opponent;
+        this.board = board;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public Player getOpponent() {
+        return opponent;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+}
+
+
