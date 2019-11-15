f661a797f9288d542c8d6fe6d7c073da61688b2e
Sam Jones <sam.jones494@googlemail.com>
2019-11-15 10:09:59 +0000

    allow spaces on board to be occupied

--- src/main/java/ttt/game/Board.java
+++ src/main/java/ttt/game/Board.java
@@ -3,10 +3,27 @@ package ttt.game;
 import java.util.Arrays;
 
 public class Board {
-    private Object[] spaces = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private Object[] spaces;
+
+    public Board() {
+        spaces = new Object[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    }
+
+    private Board(Object[] state) {
+        spaces = state;
+    }
+
+    public Object[] getSpaces() {
+        return spaces;
+    }
 
     public Object[] availableSpaces() {
         return Arrays.stream(spaces).filter(space -> space instanceof Integer).toArray();
+    }
+
+    public Board occupySpace(char symbol, Integer space) {
+        spaces[space] = symbol;
+        return new Board(spaces);
     }
 }
 
--- src/test/java/ttt/game/BoardTest.java
+++ src/test/java/ttt/game/BoardTest.java
@@ -7,6 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BoardTest {
     @Test void boardHasNineEmptySpaces() {
         Board board = new Board();
-        assertEquals(9, board.availableSpaces().length, "should have nine empty spaces");
+        assertEquals(9, board.availableSpaces().length, "should begin with nine empty spaces");
+    }
+
+    @Test void canOccupyASpace() {
+        Board board = new Board();
+        Board newBoard = board.occupySpace('X', 0);
+        assertEquals(8, newBoard.availableSpaces().length, "should be able to occupy a space");
+    }
+
+    @Test void occupiedSpacesAreMaintainedInState() {
+        Board board = new Board();
+        Board newBoard = board.occupySpace('X', 0);
+        assertEquals('X', newBoard.getSpaces()[0], "should maintain occupied spaces in state");
     }
 }
